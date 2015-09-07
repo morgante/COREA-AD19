b9e3f8fe979bf47b54b718f8fb1920173532d0ab
Morgante Pell <morgante.pell@morgante.net>
2015-09-08 01:44:32 +0400

    add basic markdown displayer

--- src/components/blog/HomeworkOne.js
+++ src/components/blog/HomeworkOne.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 var DocumentTitle = require('react-document-title');
+import {DisplayMarkdown} from '../editor/DisplayMarkdown';
 
 class HomeworkOne extends Component {
 	constructor(props) {
@@ -8,6 +9,7 @@ class HomeworkOne extends Component {
 	}
 
 	render() {
+		const text = "This is *my* text of course.";
 		if (this.props.mode === "title") {
 			return (<span>Homework 1</span>);
 		} else {
@@ -15,6 +17,7 @@ class HomeworkOne extends Component {
 				<DocumentTitle title="Homework 1">
 					<div>
 						<p>This is the first post, yay!</p>
+						<DisplayMarkdown content={text} />
 					</div>
 				</DocumentTitle>
 			);
